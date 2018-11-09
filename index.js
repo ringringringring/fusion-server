e996c7bb20c14f2bcb9c586f88ae03f88af9f306
kakegu <gusuncheng@gmail.com>
2018-11-09 09:38:44 +0800

    setting CORS

--- index.js
+++ index.js
@@ -47,6 +47,16 @@ const limiter = rateLimit({
 
 app.use(limiter);
 
+app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header("Access-Control-Allow-Headers", "access-control-allow-methods,access-control-allow-origin,content-type");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("X-Powered-By",' 3.2.1')
+    res.header("Content-Type", "application/json;charset=utf-8");
+    next();
+});
+
 const asset = require('./routers/asset')
 const account = require('./routers/account')
 
