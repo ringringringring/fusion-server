5166e452365c6a6f5c1d9505d990d6e797c8834f
kakegu <gusuncheng@gmail.com>
2018-11-22 15:04:40 +0800

    :star: Added MySQL config

---
+++ conf.js
@@ -0,0 +1,9 @@
+// MySQL
+exports.storage = 'mysql'
+exports.database = {};
+exports.database.max_connections = 200;
+exports.database.host = 'localhost';
+exports.database.name = 'uts';
+exports.database.password = '';
+exports.database.user = 'root';
+// MySQL
